4113926b093c740cd930755ec81df151865f6e72
zhaoyuwy <zhaoyuwy@163.com>
2017-03-11 19:06:30 +0800

    maven abstract

---
+++ demo1/src/main/java/test/TestCase.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import service.impl.UserService;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ * zhaoyuwy@163.com
+ */
+public class TestCase {
+    @Test
+
+    public void test() {
+
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserService us = (UserService) ac.getBean("userService");
+
+        us.save();
+
+    }
+}
